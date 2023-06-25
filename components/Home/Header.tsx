91f207feb379bcaf536e7e37334a979ad2889998
Mrutyunjaya Patra <mrutyunjayapatra@Mrutyunjayas-MacBook-Air.local>
2023-06-25 16:12:24 +0530

    complete the headerpart of home page

--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -4,7 +4,7 @@ import PersonHoldingBook from '@/public/assets/personholdingbooks.png'
 const Header = () => {
     return (
         <div className='flex md:flex-row flex-col-reverse items-center mb-4'>
-            <div className='md:pl-36 md:w-1/2 pl-4'>
+            <div className='md:pl-36 md:w-1/2 px-3'>
                 <h1 className='text-blue-600 text-4xl mt-4 md:mt-0 md:text-5xl font-medium'>Choose Your Carrer With Best College</h1>
                 <p className='leading-8 text-xl my-5 text-justify'>Choose your best Carrer. We guide you to choose best path with best College.
                     Based on your rank and score, we can show you the best college.You gurantee you to get 100% seat on your wishlist collge.
