6824a86803911ddca77406c80f6cd80382f9b650
Mrutyunjaya Patra <mrutyunjayapatra@Mrutyunjayas-MacBook-Air.local>
2023-07-11 00:50:18 +0530

    fixing the routes

--- utils/exploresclboard.ts
+++ utils/exploresclboard.ts
@@ -1,51 +1,44 @@
-import {v4 as uuid} from 'uuid'
-export const exploreschoolboard=[
+import { v4 as uuid } from 'uuid'
+export const exploreschoolboard = [
     {
-        id:uuid(),
-        title:'CBSE',
-        link:'/school',
-        data:'cbse'
+        id: uuid(),
+        title: 'CBSE',
+        href: { pathname: '/schools', query: { data: 'cbse' } }
     },
     {
-        id:uuid(),
-        title:'CHSE',
-        link:'/school',
-        data:'chse'
+        id: uuid(),
+        title: 'CHSE',
+        href: { pathname: '/schools', query: { data: 'chse' } }
     },
     {
-        id:uuid(),
-        title:'Class 11th',
-        link:'/school',
-        data:'chse'
+        id: uuid(),
+        title: 'Class 11th',
+        href: '/schools',
     },
     {
-        id:uuid(),
-        title:'Class 12th',
-        link:'/school',
-        data:'chse'
+        id: uuid(),
+        title: 'Class 12th',
+        href: '/school',
     },
     {
-        id:uuid(),
-        title:'Study Material',
-        link:'/school',
-        data:'chse'
+        id: uuid(),
+        title: 'Study Material',
+        href: '/school',
+
     },
     {
-        id:uuid(),
-        title:'Previous Year Papers',
-        link:'/school',
-        data:'chse'
+        id: uuid(),
+        title: 'Previous Year Papers',
+        href: '/school',
     },
     {
-        id:uuid(),
-        title:'Syllabus',
-        link:'/school',
-        data:'chse'
+        id: uuid(),
+        title: 'Syllabus',
+        href: '/school',
     },
     {
-        id:uuid(),
-        title:'Coaching',
-        link:'/school',
-        data:'chse'
+        id: uuid(),
+        title: 'Coaching',
+        href: '/coachings',
     },
 ]
