9c4d06b797357e8449532fd8cc23130b283d19b4
Mrutyunjaya Patra <mrutyunjayapatra@Mrutyunjayas-MacBook-Air.local>
2023-08-21 11:38:26 +0530

    temp remove feature college comp

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <Header/>
         <HomeSwipper/>
         <TopCategories/>
-        <FeaturedCollege/>
+        {/* <FeaturedCollege/> */}
         <ExploreSchoolBoard/>
       </section>
     </main>
